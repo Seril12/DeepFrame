2931126d829b360a55f463f3f5a0720367a34984
Seril Evanjaline <serilevanjaline122006@gmail.com>
2025-06-28 10:51:23 +0530

    Completed designing the UI and the voice Recognition Feature

--- documentary/src/App.jsx
+++ documentary/src/App.jsx
@@ -2,12 +2,14 @@ import Search from "./Search.jsx";
 import "./App.css";
 import Home from "./Home.jsx";
 import { useState } from "react";
+import Header from "./Header.jsx";
 
 
 function App() {
   const [query,setQuery] = useState("");
   return (
     <>
+    <Header />
     <Home setQuery = {setQuery}/>
     <Search query={query} setQuery={setQuery}/>
     </>
---
+++ documentary/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+return(
+    <div className="Header">
+        <h1>DeepFrame</h1>
+        <p>Where real stories echo louder than fiction</p>
+    </div>
+);
+}
+export default Header;
